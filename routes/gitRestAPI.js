dd6e93b78c41f406b110806d8147a0dc8d8b29b0
Abderrahmane MOUDINE <abderrahmane.moudine@gmail.com>
2022-01-31 18:54:45 +0100

    fix fetching repos by language api end point

--- routes/gitRestAPI.js
+++ routes/gitRestAPI.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { fetch_data, listRepos } = require("../controller/main");
 
-router.route("/").get(fetch_data);
-router.route("/language/:lng_name").get(listRepos);
+router.route("/repositories/:date").get(fetch_data);
+router.route("/repositories/:date/language/:lng").get(listRepos);
 module.exports = router;
